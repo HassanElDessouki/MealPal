8908c1f8655c8ff709196d6e202d5328ba5e1c15
Hassan Wael ElDessouki <hassaneldessouki@yahoo.com>
2023-09-25 21:06:10 +0300

    ReactJS Template

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/', async (req, res) => {
     res.send("<h1>Welcome to MealPal</h1>");
 });
 
+app.get('*', function(req, res) {
+    res.send("<h1>URL " + req + " is not found</h1>");
+});
+
 app.listen(80, () => {
     console.log("Listening on Port 80")
 })
