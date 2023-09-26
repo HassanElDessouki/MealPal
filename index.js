b7223bca4f7de697d8c75ebfdbff9e4e8492c025
Hassan Wael ElDessouki <hassaneldessouki@yahoo.com>
2023-09-26 15:29:14 +0300

    Added BMI Calculation Functions
    Added Input forms for Name, Age, Height and Weight. BMI is calculated and reported whether it is Underweight, Normal, Overweight or Obese.
    
    Tailwind CSS added but to be setup.
    
    ToDo: Form Validations!

--- index.js
+++ index.js
@@ -11,7 +11,23 @@ app.get("/test", (req, res) => {
 
 app.get("*", function (req, res) {
   res.send("<h1>URL " + req + " is not found</h1>");
+
+  console.log(getBMI(1.85, 104))
 });
+
+// Used to make the final report
+app.post('/userData', function(req, res) {
+  const BMI = req.body.name;
+  const age = req.body.age;
+  const height = req.body.height
+})
+
+// Takes food input from form, uses API to get more data about the food and returns it
+app.post('/searchFood', function(req, res) {
+  const BMI = req.body.name;
+  const age = req.body.age;
+  const height = req.body.height
+})
 
 app.listen(80, () => {
   console.log("Listening on " + 80);
